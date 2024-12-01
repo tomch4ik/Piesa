d192f5b463d6cefb732e7b2a58ca8493d70afee3
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-12-01 11:48:37 +0200

    dispose

---
+++ Piesa/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Piesa
+{
+    class piesa : IDisposable
+    {
+        private string name;
+        private string author;
+        private string style;
+        private int year;
+
+        public piesa(string name, string author, string style, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.style = style;
+            this.year = year;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Style
+        {
+            get { return style; }
+            set { style = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public void PiesaInfo()
+        {
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Жанр: {Style}");
+            Console.WriteLine($"Год выпуска: {Year}");
+        }
+        ~piesa()
+        {
+            Console.WriteLine($"Объект пьесы \"{Name}\" удален.");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine($"Объект пьесы \"{Name}\" удален.");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            piesa p1 = new piesa("Гамлет", "Уильям Шекспир", "Трагедия", 1603);
+            p1.PiesaInfo();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
---
+++ shop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace shop
+{
+    class Shop : IDisposable
+    {
+        private string name;
+        private string address;
+        private string type;
+
+        public Shop(string name, string address, string type)
+        {
+            this.name = name;
+            this.address = address;
+            this.type = type;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название магазина: {Name}");
+            Console.WriteLine($"Адрес магазина: {Address}");
+            Console.WriteLine($"Тип магазина: {Type}");
+        }
+
+        ~Shop()
+        {
+            Console.WriteLine($"Объект магазина \"{Name}\" удален.");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Объект магазина \"{Name}\" удален.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop("Le Silpo", "ул. Генуэзская, 24 Б", "Продовольственный");
+            shop.ShowInfo();
+            shop.Dispose();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
